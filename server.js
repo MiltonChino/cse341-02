681133c37afd368cb7f800fa641ad3e2e9bc4579
MILTON CHINO <miltonchinowrk@gmail.com>
2024-09-28 00:39:25 -0400

    team advance 1

--- server.js
+++ server.js
@@ -2,15 +2,50 @@ const express = require("express");
 const app = express();
 
 const routes = require("express").Router();
-const port = 3000;
+// const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Maria Martinez");
-});
-app.get("/helaman", (req, res) => {
-  res.send("Hello Helaman");
-});
+// MongoDB Imports
+const { MongoClient } = require("mongodb");
+const mongodb = require("./db/connect");
 
-app.listen(process.env.PORT || port, () => {
-  console.log("Web Server is listening at port " + (process.env.PORT || port));
-});
+const port = process.env.PORT || 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Maria Martinez");
+// });
+// app.get("/helaman", (req, res) => {
+//   res.send("Hello Helaman");
+// });
+
+// app.listen(process.env.PORT || port, () => {
+//   console.log("Web Server is listening at port " + (process.env.PORT || port));
+// });
+
+// MongoDB Implementation
+async function main() {
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri =
+    "mongodb+srv://adminUser:-CDDU5vGEgZa8ta@cluster0.tueaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  const client = new MongoClient(uri);
+
+  await client.connect();
+  await listDatabases(client);
+  try {
+    // check what we need here
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+main().catch(console.error);
